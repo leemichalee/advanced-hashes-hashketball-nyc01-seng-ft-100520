10b42e455523cd25b39e2d64fa2924768f7c1f21
Lee Michaeli <leeisr@gmail.com>
2020-10-01 13:10:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,13 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player, hashketball)
+  player.capitalize!
+  if hashketball[:home][:players].include?(player)
+   hashketball[:home][:players][player][:stats][:points]
+  elsif hashketball[:away][:players].include?(player)
+   hashketball[:away][:players][player][:stats][:points]
+  else
+   "No player found."
+  end
+end
